df9c8ebd8d607682193746d772bbaa4098b19b99
Eric Michaud <eric.michaud99@gmail.com>
2020-07-20 13:07:00 -0700

    custom wrappers seem to be working now

--- interp/common/wrappers.py
+++ interp/common/wrappers.py
@@ -2,6 +2,9 @@
 import numpy as np
 import scipy.ndimage
 
+import torch as th
+
+from stable_baselines3.common.type_aliases import GymStepReturn
 from stable_baselines3.common.vec_env.base_vec_env import VecEnv, VecEnvWrapper
 
 
@@ -72,4 +75,15 @@ class CustomRewardWrapper(VecEnvWrapper):
     def step_wait(self) -> 'GymStepReturn':
         observations, rewards, dones, infos = self.venv.step_wait()
         custom_rewards = self.reward_function(observations)
+        if type(custom_rewards) is th.Tensor:
+            custom_rewards = custom_rewards.cpu().detach().numpy()
+        if len(custom_rewards.shape) == 2:
+            custom_rewards = custom_rewards.flatten()
+        elif len(custom_rewards.shape) == 1:
+            pass
+        else:
+            raise Exception("Weirdly shaped reward from custom reward function")
         return observations, custom_rewards, dones, infos
+
+    def reset(self) -> np.ndarray:
+        return self.venv.reset()
