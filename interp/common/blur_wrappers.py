1bf3b4809a9dc4b244dffe450bb31165c4b5acf4
Eric Michaud <eric.michaud99@gmail.com>
2020-07-20 11:11:23 -0700

    added seaquest score blur wrapper

--- interp/common/blur_wrappers.py
+++ interp/common/blur_wrappers.py
@@ -4,17 +4,16 @@ import scipy.ndimage
 
 from stable_baselines3.common.vec_env.base_vec_env import VecEnv, VecEnvWrapper
 
-class BreakoutBlurScore(VecEnvWrapper):
-    """
-    Blurs out the score region in Breakout.
 
-    Args:
-        venv (VecEnv): An Atari Breakout environment with the standard preprocessing (a 4x framestack and 84x84 shape).
+class BlurWrapper(VecEnvWrapper):
     """
-    
-    def __init__(self, venv):
+    Base class for wrappers blurring out a region of an Atari Game.
+    """
+
+    def __init__(self, venv, mask):
         assert venv.observation_space.shape == (84, 84, 4)
-        super(BreakoutBlurScore, self).__init__(venv)
+        super(BlurWrapper, self).__init__(venv)
+        self.mask = mask
 
     @staticmethod
     def blur_score(obs: np.ndarray) -> np.ndarray:
@@ -22,13 +21,11 @@ class BreakoutBlurScore(VecEnvWrapper):
         Blur out the score region of `obs`
         """
         blurred_obs = np.zeros(obs.shape)
-        M = np.zeros((84, 84))
-        M[1:7, 18:43] = 1.
         for n in range(obs.shape[0]):
             for i in range(4):
                 frame = obs[n, :, :, i]
                 A = scipy.ndimage.gaussian_filter(frame, 3)
-                blurred = frame*(1 - M) + A*M
+                blurred = frame*(1 - self.mask) + A*self.mask
                 blurred_obs[n, :, :, i] = blurred
         return blurred_obs
 
@@ -36,7 +33,29 @@ class BreakoutBlurScore(VecEnvWrapper):
         observations, rewards, dones, infos = self.venv.step_wait()
         return self.blur_score(observations), rewards, dones, infos
 
-
     def reset(self) -> np.ndarray:
         return self.blur_score(self.venv.reset())
 
+
+class BlurBreakoutScore(BlurWrapper):
+    """
+    Blurs out the score region in Breakout.
+    """
+    
+    def __init__(self, venv):
+        assert venv.observation_space.shape == (84, 84, 4)
+        M = np.zeros((84, 84))
+        M[1:7, 18:43] = 1.
+        super(BlurBreakoutScore, self).__init__(venv, M)
+
+
+class BlurSeaquestScore(BlurWrapper):
+    """
+    Blurs out the score region in Breakout.
+    """
+    
+    def __init__(self, venv):
+        assert venv.observation_space.shape == (84, 84, 4)
+        M = np.zeros((84, 84))
+        M[3:7, 38:56] = 1.
+        super(BlurBreakoutScore, self).__init__(venv, M)
