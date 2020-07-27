6a8f93007f75167e0fdd7de138b97ea734e997fd
Eric Michaud <eric.michaud99@gmail.com>
2020-07-27 13:59:36 -0700

    possibly working DRLHP test script for pong, synthetic preferences

--- scripts/synthetic_preferences_test.py
+++ scripts/synthetic_preferences_test.py
@@ -1,44 +1,62 @@
 
 import sys
+import pathlib
 import logging
 
+import numpy as np
+import matplotlib.pyplot as plt
+
 import gym
+from gym.spaces import Box
+from gym.wrappers import AtariPreprocessing, FrameStack
 
+from stable_baselines3 import A2C, PPO, DQN
 from stable_baselines3.common.cmd_util import make_atari_env
-from stable_baselines3.common.vec_env import VecFrameStack, DummyVecEnv
-from stable_baselines3 import A2C
-from stable_baselines3.common.atari_wrappers import AtariWrapper
+from stable_baselines3.common.vec_env import VecFrameStack
 
-
-#sys.path.insert(1, "/home/eric/Code/chai/test/learning-from-human-preferences")
-#sys.path.insert(2, "/home/eric/Code/chai/test/learning-from-human-preferences/drlhp")
 from drlhp import HumanPreferencesEnvWrapper
 from drlhp.reward_predictor_core_network import net_cnn
 
-#import warnings
-#with warnings.catch_warnings():
-#    warnings.simplefilter("ignore")
-#    import easy_tf_log
-##    from drlhp import reward_predictor
-#    from drlhp.reward_predictor_core_network import net_cnn
-##    from drlhp.HumanPreferencesEnvWrapper import _make_reward_predictor
-#    from drlhp import HumanPreferencesEnvWrapper
 
+class VanillaFrameStack(FrameStack):
+    """Stacks frames but without using LazyFrame."""
+
+    def __init__(self, env, n_stack=4):
+        """ 
+        Wraper for `env` which stacks `n_stack` frames.
+
+        Args:
+            env (gym.Env): Environment to wrap
+            n_stack (int): Number of observations to stack
+        """
+        super(VanillaFrameStack, self).__init__(env, n_stack)
+        self.observation_space = Box(0, 255, shape=[84, 84, 4], dtype=env.observation_space.dtype)
+
+    def _get_observation(self):
+        assert len(self.frames) == self.num_stack, (len(self.frames), self.num_stack)
+        stack = np.array(list(self.frames))         # (4, 84, 84)
+        stack = np.transpose(stack, axes=(1, 2, 0)) # (84, 84, 4)
+        return stack
 
 
 if __name__ == '__main__':
+    save_dir = Path() / 'drlhp_pong'
+    if not save_dir.exists():
+        save_dir.mkdir()
     env = gym.make('PongNoFrameskip-v4')
-    env = AtariWrapper(env)
-    env = DummyVecEnv([lambda: env])
-    #env = VecFrameStack(env, n_stack=4)
-#    env = make_atari_env('PongNoFrameskip-v4', n_envs=1, seed=0)
-#    env = VecFrameStack(env, n_stack=4)
-    env.reward_range = (float('-inf'), float('inf'))
-    #preferences_env = env
+    env = AtariPreprocessing(env, frame_skip=1)
+    obs = env.reset()
+    print(type(obs), obs.shape)
+    env = VanillaFrameStack(env, n_stack=4)
+    obs = env.reset()
+    print(type(obs), obs.shape)
     preferences_env = HumanPreferencesEnvWrapper(env=env,
                             reward_predictor_network=net_cnn,
-                            synthetic_prefs=True)
-    #preferences_env = DummyVecEnv([lambda: preferences_env]) 
+                            segment_length=100,
+                            synthetic_prefs=True,
+                            log_dir: str = "drlhp-pong/drlhp_logs/"
+                            n_initial_training_steps=0)
     model = A2C('CnnPolicy', preferences_env, verbose=1)
-    model.learn(total_timesteps=int(1e5))
+    model.learn(total_timesteps=int(1e6), eval_env=env)
+    model.save(save_dir / 'model.zip')
 
