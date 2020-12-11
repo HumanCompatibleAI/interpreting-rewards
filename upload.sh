fe28eda4df9a86ca27d761d0332fa8539a984e7f
Eric Michaud <eric.michaud99@gmail.com>
2020-12-10 20:05:43 -0800

    removed yet more depreciated files

--- upload.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-aws s3 cp agents s3://interpreting-rewards/eric-dev/agents --recursive
-aws s3 cp videos s3://interpreting-rewards/eric-dev/videos --recursive
-aws s3 cp reward-models s3://interpreting-rewards/eric-dev/reward-models --recursive
-
-
-
-
